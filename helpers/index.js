6f344eb8d277ea2230435afbb378450f00a5ff82
Mauricio Nunes dos Reis <mauriciotipo_0@hotmail.com>
2015-09-21 10:25:19 -0300

    helpers

---
+++ helpers/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+	String:String
+}
+if (!String.prototype.Format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
+}
